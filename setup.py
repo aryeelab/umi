f732eb1f3449d5f6a74ade3fa82e3e78356858d6
Ved Topkar <vedtopkar@gmail.com>
2015-09-24 13:44:46 -0400

    Added files for travis

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(name='UMI',
+      version='1.0',
+      description='Python Distribution Utilities',
+      author='Martin Aryee',
+      author_email='',
+      url='https://github.com/aryeelab/umi',
+      install_requires=[
+        'htseq'
+      ]
+     )
